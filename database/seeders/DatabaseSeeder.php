e9ff0c5f4ce0d29f76016285fd939e04d993ef10
saliou-t <zalltalla46@gmail.com>
2023-11-07 14:26:41 +0000

    feat:  mise en place de la table 'tache' et des seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tache;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        
+        
+        Tache::factory(30)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/tache/taches.blade.php
+++ resources/views/tache/taches.blade.php
@@ -1,18 +1,15 @@
 @include('default')
-
 <div class="container">
     <div class="row">
         <div class="col">
             <h1 class="my-5">
                 Liste des taches
             </h1>
-
         </div>
     </div>
 
     <div class="container">
         <div class="row">
-            
             @for ($i = 1; $i < 8; $i++)
                 <div class="col-12 col-md-4 my-1">
                     <div class="card mx-5" style="width: 18rem;">
@@ -27,7 +24,6 @@
                     </div>
                 </div>
             @endfor
-
         </div>
     </div>
 </div>
