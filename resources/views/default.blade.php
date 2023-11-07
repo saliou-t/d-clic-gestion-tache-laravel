0858ebf4d85907b69f42a4854c18a55b7902e5c1
saliou-t <zalltalla46@gmail.com>
2023-11-07 12:11:42 +0000

    initial commit, feat : creation de la page pour afficher les taches

---
+++ resources/views/default.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Gestion Taches</title>
+    <link rel="stylesheet" href="https://bootswatch.com/5/sketchy/bootstrap.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03"
+                aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor03">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Taches
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nouvelle</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-sm-2" type="search" placeholder="Chercher quelque choses ">
+                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+                </form>
+            </div>
+        </div>
+    </nav>
---
+++ resources/views/tache/taches.blade.php
@@ -0,0 +1,33 @@
+@include('default')
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="my-5">
+                Liste des taches
+            </h1>
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            
+            @for ($i = 1; $i < 8; $i++)
+                <div class="col-12 col-md-4 my-1">
+                    <div class="card mx-5" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Nom de la tache {{ $i}} </h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Priorité : haute</h6>
+                            <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab,
+                                cupiditate. Soluta hic odit. Odio quisquam ea doloribus ?</p>
+                            <a href="#" class="card-link">supprimer</a>
+                            <a href="#" class="card-link">details</a>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+
+        </div>
+    </div>
+</div>
